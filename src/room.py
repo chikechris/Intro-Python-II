3c55703451df93a57e73890304e735de481d101f
chikechris <cchike2017@gmail.com>
2019-12-04 14:48:49 -0500

    Added direction_to_move_in function

--- src/room.py
+++ src/room.py
@@ -1,6 +1,24 @@
+
+
 # Implement a class to hold room information. This should have name and
 # description attributes.
 class Room:
     def __init__(self, name, description):
         self.name = name                  # room name
         self.description = description    # indicates what the player sees and feels
+        self.n_to = None
+        self.s_to = None
+        self.e_to = None
+        self.w_to = None
+
+    def direction_to_move_in(self, direction):
+        if direction == "N":
+            return self.n_to
+        elif direction == "S":
+            return self.s_to
+        elif direction == "E":
+            return self.e_to
+        elif direction == "W":
+            return self.w_to
+        else:
+            return None
